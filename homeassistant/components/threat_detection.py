eb0381d7aeb5bf676288719603eac7de3a24aac9
Lisa Lipkin <lipkin@student.chalmers.se>
2018-06-18 15:16:32 +0200

    Test to see if the component will write to logs.

--- homeassistant/components/threat_detection.py
+++ homeassistant/components/threat_detection.py
@@ -4,8 +4,12 @@ Component for detecting threats against the smart home.
 For more information on this component see \todo add where to find documontation for the component.
 """
 
+import logging
+
 COMPONENT = 'threat_detection'
 
-def setup(hass, config):
+def setup(hass, config=None):
     """Set up the threat_detection component."""
+    log = logging.getLogger(__name__)
+    log.info("""This is a test to se if the threat_detection component can write to the logs""")
     return True

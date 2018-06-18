e7acac8e3a3ec1c8cbfc270b22473fb9e7b8fad0
Lisa Lipkin <lipkin@student.chalmers.se>
2018-06-18 16:13:53 +0200

    Support for logging is functional.

--- homeassistant/components/threat_detection.py
+++ homeassistant/components/threat_detection.py
@@ -7,12 +7,16 @@ For more information on this component see \todo add where to find documontation
 import asyncio
 import logging
 
+_LOGGER = logging.getLogger(__name__)
+
 DOMAIN = 'threat_detection'
 
 @asyncio.coroutine
 def async_setup(hass, config=None):
     """Set up the threat_detection component."""
-    log = logging.getLogger(__name__)
-    log.info("""This is a test to se if the threat_detection component can write to the logs""")
+
+
+
+    _LOGGER.info("The threat_detection component is running!")
 
     return True

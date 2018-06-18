dc797529fc0e411f97468155b3629588db351b66
Lisa Lipkin <lipkin@student.chalmers.se>
2018-06-18 15:47:47 +0200

    Changed COMPONENT to DOMAIN to fix HA finding it.

--- homeassistant/components/threat_detection.py
+++ homeassistant/components/threat_detection.py
@@ -6,10 +6,12 @@ For more information on this component see \todo add where to find documontation
 
 import logging
 
-COMPONENT = 'threat_detection'
+DOMAIN = 'threat_detection'
 
 def setup(hass, config=None):
     """Set up the threat_detection component."""
     log = logging.getLogger(__name__)
     log.info("""This is a test to se if the threat_detection component can write to the logs""")
+
+    hass.
     return True

802b2ccffa57f6e1bcf8e4b1c94dc642b774f468
Lisa Lipkin <lipkin@student.chalmers.se>
2018-06-18 13:57:40 +0200

    Initial commit for the threat detection component. It has no functionallity.

---
+++ homeassistant/components/threat_detection.py
@@ -0,0 +1,10 @@
+"""
+Component for detecting threats against the smart home.
+
+For more information on this component see \todo add where to find documontation for the component.
+"""
+
+COMPONENT = 'threat_detection'
+
+def setup(hass, config):
+    return True

6d6d6e81d2f74e153035299ff1d732e3d2d3d092
Lisa Lipkin <lipkin@student.chalmers.se>
2018-06-19 12:30:14 +0200

    Followed the tut on how to make it write stuff in the gui. Probably won't work.

--- homeassistant/components/threat_detection.py
+++ homeassistant/components/threat_detection.py
@@ -6,16 +6,28 @@ For more information on this component see \todo add where to find documontation
 
 import asyncio
 import logging
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'threat_detection'
+DEPENDENCIES = []
+
+CONF_TEXT = 'text'
+DEFAULT_TEXT = 'No text!'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_TEXT):cv.string,
+})
+}, extra = vol.ALLOW_EXTRA)
 
 @asyncio.coroutine
 def async_setup(hass, config=None):
     """Set up the threat_detection component."""
-
-
+    text = config[DOMAIN].get(CONF_TEXT, DEFAULT_TEXT)
+    hass.states.set('threat_detection.Threat_State', text)
 
     _LOGGER.info("The threat_detection component is running!")
 

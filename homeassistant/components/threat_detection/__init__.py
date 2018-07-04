4f151a13aca6590c899cea53652253e035f1ea53
Lisa Lipkin <lipkin@student.chalmers.se>
2018-07-04 14:08:01 +0200

    Added an identifying string in the print_dict so that it can be found in the log.

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -37,6 +37,8 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 DEVICES = {}
+
+
 @asyncio.coroutine
 def async_setup(hass, config=None):
     """Set up the threat_detection component."""
@@ -53,40 +55,40 @@ def async_setup(hass, config=None):
         'threat_detection.Threats_Detected', DEFAULT_DETECTIONS)
     hass.states.async_set('threat_detection.Input', userinput)
 
-    setup_devices(yield from hass.components.device_tracker.async_load_config(
-        os.path.join(hass.config.config_dir, KNOWN_DEVICES), hass, 0))
-    for device in devices:
-        _LOGGER.info(
-            "DEVICE_SCAN: device %s has mac %s", device.entity_id, device.mac)
+    devices = yield from hass.components.device_tracker.async_load_config(
+        os.path.join(hass.config.config_dir, KNOWN_DEVICES), hass, 0)
+    setup_devices(devices)
 
     _LOGGER.info("The threat_detection component is set up!")
 
-
     return True
 
+
 def setup_devices(devices):
+    """Sets up the data structure for the information about the devices."""
     for device in devices:
-        DEVICES.update({device.mac:device.entity_id})
+        DEVICES.update({device.mac: device.entity_id})
 
     print_dict(DEVICES)
 
+
 def print_dict(obj):
-    if type(obj) == dict:
-        for k, v in obj.items():
-            if hasattr(v, '__iter__'):
-                _LOGGER.info(k)
-                print_dict(v)
+    """Prints a dict to log, may or may not be done prettily."""
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if hasattr(value, '__iter__'):
+                _LOGGER.info("KEY = %s", key)
+                print_dict(value)
             else:
-                _LOGGER.info('%s : %s', k, v)
-    elif type(obj) == list:
-        for v in obj:
-            if hasattr(v, '__iter__'):
-                print_dict(v)
+                _LOGGER.info('%s : %s', key, value)
+    elif isinstance(obj, list):
+        for value in obj:
+            if hasattr(value, '__iter__'):
+                print_dict(value)
             else:
-                _LOGGER.info(v)
+                _LOGGER.info(value)
     else:
         _LOGGER(obj)
-
 
 
 # @property

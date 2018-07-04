a03bba05832dc24bedb2aa7c83a36c48db227d87
Lisa Lipkin <lipkin@student.chalmers.se>
2018-07-04 13:56:48 +0200

    Trying to find a way to save and access the device data that we are interested in.

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -36,7 +36,7 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
-
+DEVICES = {}
 @asyncio.coroutine
 def async_setup(hass, config=None):
     """Set up the threat_detection component."""
@@ -53,15 +53,40 @@ def async_setup(hass, config=None):
         'threat_detection.Threats_Detected', DEFAULT_DETECTIONS)
     hass.states.async_set('threat_detection.Input', userinput)
 
-    devices = yield from hass.components.device_tracker.async_load_config(
-        os.path.join(hass.config.config_dir, KNOWN_DEVICES), hass, 0)
+    setup_devices(yield from hass.components.device_tracker.async_load_config(
+        os.path.join(hass.config.config_dir, KNOWN_DEVICES), hass, 0))
     for device in devices:
         _LOGGER.info(
             "DEVICE_SCAN: device %s has mac %s", device.entity_id, device.mac)
 
     _LOGGER.info("The threat_detection component is set up!")
 
+
     return True
+
+def setup_devices(devices):
+    for device in devices:
+        DEVICES.update({device.mac:device.entity_id})
+
+    print_dict(DEVICES)
+
+def print_dict(obj):
+    if type(obj) == dict:
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                _LOGGER.info(k)
+                print_dict(v)
+            else:
+                _LOGGER.info('%s : %s', k, v)
+    elif type(obj) == list:
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                print_dict(v)
+            else:
+                _LOGGER.info(v)
+    else:
+        _LOGGER(obj)
+
 
 
 # @property

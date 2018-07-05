5ac4c3d6a28c26c28aacaeff6aa0a3fcb9c22199
Matz Larsson <matzl@student.chalmers.se>
2018-07-05 12:56:09 +0200

    Added support for network packet capture (experimental)

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -5,6 +5,10 @@ For more information on this component see
 todo add where to find documontation for the component.
 """
 
+import sys
+import time
+import os
+from os.path import dirname, basename, isfile, join
 import asyncio
 import logging
 import voluptuous as vol
@@ -47,13 +51,68 @@ def async_setup(hass, config=None):
     hass.states.async_set(
         'threat_detection.Threats_Detected', DEFAULT_DETECTIONS)
     hass.states.async_set('threat_detection.Input', userinput)
+    
+    # Start capturing packets from network
+    capturer = PacketCapturer(join(hass.config.config_dir, "traces"))
+    capturer.add_callback(on_network_capture)
 
     _LOGGER.info("The threat_detection component is set up!")
 
     return True
+    
+def on_network_capture(packet_list):
+    _LOGGER.info(packet_list)
 
 
 # @property
 # def state_attributes(self):
 #     """Return state attributes of the component"""
 #     return self._attributes
+
+
+class PacketCapturer:
+
+    from watchdog.events import FileSystemEventHandler
+
+    def __init__(self, path):
+        self.callbacks = []
+        from watchdog.observers import Observer
+        self.observer = Observer()
+        self.observer.schedule(self.PacketCaptureHandler(self.on_event), path)
+        self.observer.start()
+        
+    def on_event(self, packet_list):
+        for callback in self.callbacks:
+            callback(packet_list)
+            
+    def add_callback(self, callback):
+        if callback is not None:
+            self.callbacks.append(callback)
+
+    def __del__(self):
+        if self.observer is not None:
+            self.observer.stop()
+            self.observer.join()
+            self.observer = None
+            
+    class PacketCaptureHandler(FileSystemEventHandler):
+
+        def __init__(self, callback):
+            super(PacketCapturer.PacketCaptureHandler, self).__init__()
+            self.callback = callback
+
+        def on_created(self, event):
+            from scapy.all import rdpcap, PacketList
+            path = dirname(event.src_path)
+            all_files = [f for f in os.listdir(path) if isfile(join(path, f))]
+            files = list(filter(self.file_filter(event.src_path), all_files))
+            data = [rdpcap(join(path, file)) for file in files]
+            for file in files:
+                os.remove(join(path, file))
+            self.callback(PacketList([pkt for pkts in data for pkt in pkts]))
+                
+        def file_filter(self, ignore_file):
+            def f_filter(f):
+                return f.endswith('.pcap') and f != basename(ignore_file)
+            return f_filter
+

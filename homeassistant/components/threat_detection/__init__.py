214b3c6ac768d82c993099cde16ea9328cb51ebb
Matz Larsson <matzl@student.chalmers.se>
2018-07-16 16:38:13 +0200

    Added first iteration of profiling framework

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -9,6 +9,8 @@ import sys
 import time
 import os
 from os.path import dirname, basename, isfile, join
+from datetime import datetime, timedelta
+import yaml
 import asyncio
 import logging
 import voluptuous as vol
@@ -36,6 +38,9 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 CAPTURER = None
+PROFILES = {}
+IGNORE_LIST = ['ff:ff:ff:ff:ff:ff', '2c:4d:54:75:05:10']
+STORAGE_NAME = join(hass.config.config_dir, 'td_profiles.yaml')
 
 
 @asyncio.coroutine
@@ -58,6 +63,8 @@ def async_setup(hass, config=None):
     global CAPTURER
     CAPTURER = PacketCapturer(join(hass.config.config_dir, "traces"))
     CAPTURER.add_callback(on_network_capture)
+    load_profiles()
+    add_profile_callbacks()
 
     _LOGGER.info("The threat_detection component is set up!")
 
@@ -66,6 +73,7 @@ def async_setup(hass, config=None):
 def on_network_capture(packet_list):
     """Called when a network packet list has been captured """
     _LOGGER.info(packet_list)
+    transfer_data_to_profile(packet_list)
 
 
 # @property
@@ -74,6 +82,7 @@ def on_network_capture(packet_list):
 #     return self._attributes
 
 
+""" Handling of network traffic """
 class PacketCapturer:
     """Reads network packet captures and provides a way to register
        callbacks to receive this data """
@@ -135,3 +144,97 @@ class PacketCapturer:
                 return f.endswith('.pcap') and f != basename(ignore_file)
             return f_filter
 
+            
+
+""" Handling of profiling """
+def load_profiles():
+    try:
+        with open(STORAGE_NAME, 'r') as infile:
+            indata = yaml.load(infile)
+        for mac, prof in indata:
+            if assure_profile_exists(mac):
+                PROFILES[mac].profiling_end = prof.prof_end
+                PROFILES[mac].profile = prof.prof
+    except FileNotFoundError:
+        # Will happen on first run due to no previous save file. 
+        pass
+
+def store_profiles():
+    outdata = {}
+    for mac, prof in PROFILES:
+        outdata[mac] = {prof_end: prof.profiling_end, prof: prof.profile}
+    with open(STORAGE_NAME, 'w') as outfile:
+        yaml.dump(outdata, outfile, default_flow_style=False)
+
+def assure_profile_exists(mac):
+    if not mac in IGNORE_LIST:
+        if PROFILES.get(mac) is None:
+            PROFILES[mac] = Profile(mac)
+        return True
+    else:
+        return False
+
+def transfer_data_to_profile(packets):
+    for packet in packets:
+        if packet.haslayer("Ether"):
+            feed_profile_data(packet.getlayer("Ether").src, packet)
+            feed_profile_data(packet.getlayer("Ether").dst, packet)
+        else:
+            _LOGGER.debug("Unknown packet: "+packet.summary())
+
+def feed_profile_data(mac, packet):
+    if assure_profile_exists(mac):
+        response = PROFILES[mac].handle_packet(packet)
+        if response is not None:
+            _LOGGER.warning("THREAT WARNING: "+str(response))
+
+
+class Profile(object):
+
+    updaters = []
+    checkers = []
+
+    @staticmethod
+    def add_updater(callback):
+        if callback is not None:
+            Profile.updaters.append(callback)
+
+    @staticmethod
+    def add_checker(callback):
+        if callback is not None:
+            Profile.checkers.append(callback)
+
+    def __init__(self, mac):
+        self.mac = mac
+        self.profiling_end = datetime.now()+timedelta(days=1)
+        self.profile = {}
+
+    def is_profiling(self):
+        return datetime.now() < self.profiling_end
+
+    def handle_packet(self, packet):
+        if self.is_profiling():
+            self.update(packet)
+        else:
+            self.check(packet)
+
+    def update(self, packet):
+        [updater(self, packet) for updater in Profile.updaters]
+
+    def check(self, packet):
+        checks = (checker(self, packet) for checker in Profile.checkers)
+        errors = [check for check in checks if check is not None]
+        return errors if len(errors) != 0 else None
+
+    def set(self, key, value):
+        self.profile[key] = value
+
+    def get(self, key, default_val=None):
+        return self.profile.get(key, default_val)
+        
+        
+def add_profile_callbacks():
+    Profile.add_updater(update_whitelist)
+    
+    
+    

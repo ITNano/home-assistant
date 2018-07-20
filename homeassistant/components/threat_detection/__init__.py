bc50c14028a9e41a83c8a38cfa50aca219bf6e40
Matz Larsson <matzl@student.chalmers.se>
2018-07-20 11:49:28 +0200

    Removed some excessive logging

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -370,10 +370,8 @@ def update_whitelist_dns(profile, pkt):
     if pkt.haslayer("DNS"):
         is_sender = check_if_sender(profile, pkt)
         if is_sender:
-            _LOGGER.info("Handling DNS packet")
+            _LOGGER.info("Handling DNS packet: " + dnsp.summary())
             dnsp = pkt.getlayer("DNS")
-            _LOGGER.info("AN count: " + str(dnsp.ancount))
-            _LOGGER.info(dnsp.summary())
             if dnsp.ancount > 0:
                 records = [pkt.getlayer("DNSRR")[i] for i in range(dnsp.ancount)]
                 domain = records[0].rdata

55ea9d5d517ce7f797f6a3dac601cc685b8c9a95
Matz Larsson <matzl@student.chalmers.se>
2018-09-21 19:26:21 +0200

    Syntax error

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -162,7 +162,7 @@ def async_load_device_data(hass):
                                      'name': device.name}})
 
         # Backwards compat (add devices already existing)
-        _LOGGER.info("Using meta data for " + str(id)+" (" + str(DEVICES[id])+") " + PROFILES.get(id))
+        _LOGGER.info("Using meta data for " + str(id)+" (" + str(DEVICES[id])+") " + str(PROFILES.get(id)))
         if PROFILES.get(id):
             _LOGGER.info("Setting data")
             for prop in DEVICES[id]:

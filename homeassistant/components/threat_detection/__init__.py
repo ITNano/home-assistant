ce4c025fdd9a08f8b3ba568b163a2bb8c2dfc848
Matz Larsson <matzl@student.chalmers.se>
2018-09-21 15:10:39 +0200

    Debugging ignore list

--- homeassistant/components/threat_detection/__init__.py
+++ homeassistant/components/threat_detection/__init__.py
@@ -324,6 +324,7 @@ def find_profiles(sender, receiver):
 
 def get_profile(id):
     """Retrieves/creates the profile with the given ID"""
+    _LOGGER.info(IGNORE_LIST)
     if id not in IGNORE_LIST:
         if PROFILES.get(id) is None:
             PROFILES[id] = Profile(id)

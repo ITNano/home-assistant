5d19efa4bb64ec9a91d724f9f684570a46d36365
Matz Larsson <matzl@student.chalmers.se>
2018-10-08 15:50:13 +0200

    Debugging why module isnt loaded

--- homeassistant/components/threat_detection/all.py
+++ homeassistant/components/threat_detection/all.py
@@ -25,7 +25,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 DEFAULT_PLATFORMS = ['botnet']
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the platform."""
     platforms = DEFAULT_PLATFORMS
     if config.get(CONF_EXCLUDE):

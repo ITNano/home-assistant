5820ecc3b1a49d5eb2f2c6a1cd60e423693bf830
Matz Larsson <matzl@student.chalmers.se>
2018-10-12 19:33:44 +0200

    Reversed

--- homeassistant/components/threat_detection/eviltwin.py
+++ homeassistant/components/threat_detection/eviltwin.py
@@ -73,7 +73,6 @@ def analyse_mix(profile, packet):
     # Just check that all data exists
     if not profile.data.get("rssi_max"):
         _LOGGER.warning("Found eviltwin profile without profile end data")
-        on_profiling_end_mix(profile)
 
     # Do analyse
     from scapy.all import RadioTap, Dot11Elt
